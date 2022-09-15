93f4d1bdca4ff8a7a9249970ddf8e8964eb7c91e
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-09-15 10:00:18 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -7,9 +7,7 @@ st.title('Havbøye')
 
  
 def start():
-    response=send_at_get_resp("AT+CGNSINF")
-    print("Ulstein TOF2 Bøye nr1 "+response)
-    file=open("data.csv","a")
+    file=open("Havbøye-data.csv","a")
     file.write(str(response)+",")
     file.flush()
     file.close()
