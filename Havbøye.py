0be847c8c0ccb64b6df1329f3d222027bb761bbd
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-09-19 14:01:25 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -1,7 +1,7 @@
 #Import the required Libraries
 import streamlit as st
 import pandas as pd
-import matplotlib_pyplot as plt
+import matplotlibpyplot as plt
 # Add a title and intro text
 st.title('Earthquake Data Explorer')
 st.text('This is a web app to allow exploration of Earthquake Data')
