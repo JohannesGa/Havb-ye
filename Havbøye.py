3850a88cbb84a58656a066c05f4f08fda306ec91
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-09-19 13:59:08 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -1,8 +1,27 @@
+#Import the required Libraries
 import streamlit as st
 import pandas as pd
-import numpy as np
-
-st.title('Havbøye')
-st.subheader('Data frå havbøye')
-
-
+import matplotlib.pyplot as plt
+# Add a title and intro text
+st.title('Earthquake Data Explorer')
+st.text('This is a web app to allow exploration of Earthquake Data')
+# Create file uploader object
+upload_file = st.file_uploader('Upload a file containing earthquake data')
+# Check to see if a file has been uploaded
+if upload_file is not None:
+   # If it has then do the following:
+   # Read the file to a dataframe using pandas
+   df = pd.read_csv(upload_file)
+   # Create a section for the dataframe statistics
+   st.header('Statistics of Dataframe')
+   st.write(df.describe())
+   # Create a section for the dataframe header
+   st.header('Header of Dataframe')
+   st.write(df.head())
+   # Create a section for matplotlib figure
+   st.header('Plot of Data')
+   fig, ax = plt.subplots(1,1)
+   ax.scatter(x=df['Depth'], y=df['Magnitude'])
+   ax.set_xlabel('Depth')
+   ax.set_ylabel('Magnitude')
+   st.pyplot(fig)
