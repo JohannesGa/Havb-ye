57f804eef0daa32f7d83751b0e1634b23b62d2f1
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-10-24 10:08:09 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -12,32 +12,6 @@ st.text('Informasjon henta frå havbøye')
 upload_file = st.file_uploader('Last opp data om Havbøya')
 # Check to see if a file has been uploaded
 
-'''
-if upload_file is not None:
-   # If it has then do the following:
-   # Read the file to a dataframe using pandas
-   df = pd.read_csv(upload_file)
-   # Create a section for the dataframe statistics
-   st.header('Statistics of Dataframe')
-   st.write(df.describe())
-   # Create a section for the dataframe header
-   st.header('Header of Dataframe')
-   st.write(df.head())
-'''
-'''   
-url = 'http://sensor.marin.ntnu.no/logs/Gruppe15.csv'
-request = requests.get(url)
-print(request.text)
-'''
-'''
-   # Kart 
-df = pd.DataFrame(
-   np.random.randn(1, 2) / [50, 50] + [62.3433, 5.8488],
-   columns=['lat', 'lon'])
-
-st.map(df)
-'''
-
 headers = []
 
 
