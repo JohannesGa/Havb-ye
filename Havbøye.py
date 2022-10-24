be8c9c3e2d56ee48db757d47a6e934ca44cf2ae0
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-10-24 10:10:49 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -17,7 +17,7 @@ headers = []
 
 
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'long','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','loc', 'time', 'lat', 'long','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
 st.header("Bar_chart_test")
 #st.bar_chart(df[['time','lat','long']])
 st.dataframe(df)
@@ -33,7 +33,7 @@ headers = []
 
 
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein17_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
 st.header("Bar_chart_test")
 #st.bar_chart(df[['lat','lon']])
 st.dataframe(df)
