98366b308fbbacaf7b6ba72d66ab0d65d93b8aec
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-10-26 09:34:22 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -11,7 +11,7 @@ headers = []
 
 st.title('Havbøye prosjekt')
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3'])
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB1.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3'])
 
 st.header('Informasjon om bøye 4')
 st.dataframe(df)
