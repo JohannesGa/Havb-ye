61991e74327667079afa46290ab409e3259c8005
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-10-27 13:21:41 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -9,9 +9,33 @@ headers = []
 
 st.title('Havbøye prosjekt')
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3'])
+df1 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB1.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
+df2 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
+df3 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB3.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
+df4 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB4.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
+df5 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 
 st.header('Informasjon om bøye 4')
-st.dataframe(df)
+st.dataframe(df1)
 
-st.map(df)
+st.map(df1)
+
+st.header('Informasjon om bøye 4')
+st.dataframe(df2)
+
+st.map(df2)
+
+st.header('Informasjon om bøye 4')
+st.dataframe(df3)
+
+st.map(df3)
+
+st.header('Informasjon om bøye 4')
+st.dataframe(df4)
+
+st.map(df4)
+
+st.header('Informasjon om bøye 4')
+st.dataframe(df5)
+
+st.map(df5)
