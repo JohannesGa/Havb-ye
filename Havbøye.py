46533f48267fc07ebd90afd91594ed19c0d0279e
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-10-27 14:12:05 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -17,23 +17,24 @@ df5 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5.txt", names = ['fi
 st.header('Informasjon om bøye 1')
 st.dataframe(df1)
 
-
+st.map(df1)
 
 st.header('Informasjon om bøye 2')
 st.dataframe(df2)
 
+st.map(df2)
 
 st.header('Informasjon om bøye 3')
 st.dataframe(df3)
 
-
+st.map(df3)
 
 st.header('Informasjon om bøye 4')
 st.dataframe(df4)
 
-
+st.map(df4)
 
 st.header('Informasjon om bøye 5')
 st.dataframe(df5)
 
-
+st.map(df5)
