302df87e154c1ff9f85445ea1a13b1aac5881f5d
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-10-27 13:35:04 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -17,25 +17,45 @@ df5 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5.txt", names = ['fi
 
 st.header('Informasjon om bøye 1')
 st.dataframe(df1)
-
 st.map(df1)
+
+url1 = 'http://sensor.marin.ntnu.no/logs/UlsteinB1.txt'
+
+if st.button('Data fra maritim'):
+    webbrowser.open_new_tab(url1) 
 
 st.header('Informasjon om bøye 2')
 st.dataframe(df2)
-
 st.map(df2)
+
+url2 = 'http://sensor.marin.ntnu.no/logs/UlsteinB2.txt'
+
+if st.button('Data fra maritim'):
+    webbrowser.open_new_tab(url2)
 
 st.header('Informasjon om bøye 3')
 st.dataframe(df3)
-
 st.map(df3)
+
+url3 = 'http://sensor.marin.ntnu.no/logs/UlsteinB3.txt'
+
+if st.button('Data fra maritim'):
+    webbrowser.open_new_tab(url3)
 
 st.header('Informasjon om bøye 4')
 st.dataframe(df4)
-
 st.map(df4)
+
+url4 = 'http://sensor.marin.ntnu.no/logs/UlsteinB4.txt'
+
+if st.button('Data fra maritim'):
+    webbrowser.open_new_tab(url4)
 
 st.header('Informasjon om bøye 5')
 st.dataframe(df5)
-
 st.map(df5)
+
+url5 = 'http://sensor.marin.ntnu.no/logs/UlsteinB5.txt'
+
+if st.button('Data fra maritim'):
+    webbrowser.open_new_tab(url5)
