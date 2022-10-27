9ea6ac56178cbfce0545919c262a427c5517e74e
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-10-27 13:22:30 +0200

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -15,17 +15,17 @@ df3 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB3.txt", names = ['fi
 df4 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB4.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 df5 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 
-st.header('Informasjon om bøye 4')
+st.header('Informasjon om bøye 1')
 st.dataframe(df1)
 
 st.map(df1)
 
-st.header('Informasjon om bøye 4')
+st.header('Informasjon om bøye 2')
 st.dataframe(df2)
 
 st.map(df2)
 
-st.header('Informasjon om bøye 4')
+st.header('Informasjon om bøye 3')
 st.dataframe(df3)
 
 st.map(df3)
@@ -35,7 +35,7 @@ st.dataframe(df4)
 
 st.map(df4)
 
-st.header('Informasjon om bøye 4')
+st.header('Informasjon om bøye 5')
 st.dataframe(df5)
 
 st.map(df5)
