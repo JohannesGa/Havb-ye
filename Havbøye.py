1fa411c83c7e698a1bbd8d879d4800b325498706
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2022-12-05 11:10:13 +0100

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -8,16 +8,16 @@ headers = []
 
 st.title('Havbøye prosjekt')
 
-#df1 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB1.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
+df1 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB1T2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 df2 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB2T2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 df3 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB3T2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 df4 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB4T2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 df5 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5T1.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 
-#st.header('Informasjon om bøye 1')
-#st.dataframe(df1)
+st.header('Informasjon om bøye 1')
+st.dataframe(df1)
 
-#st.map(df1)
+st.map(df1)
 
 st.header('Informasjon om bøye 2')
 st.dataframe(df2)
