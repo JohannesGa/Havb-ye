f9331f167b82715167e342f926273fbad68ba3c1
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2023-01-23 15:30:22 +0100

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -10,7 +10,7 @@ st.title('Havbøye prosjekt')
 
 df1 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB1T4%253E.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 #df2 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB2T2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
-#df3 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB3T2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
+df3 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB3T4.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 #df4 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB4T2.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 #df5 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5T1.txt", names = ['filename', 'power','location', 'time', 'lat', 'lon','Altitude','Speed','Course','Fix Mode','Reserved1','HDOP','PDOP','VDOP','Reserved2','GPS','GNSS','GLONASS','Reserved3','Temperature'])
 
