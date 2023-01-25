d22d45f36eff3cd5e5254ef866ffdacd79415633
JohannesGa <113115540+JohannesGa@users.noreply.github.com>
2023-01-25 20:24:47 +0100

    Update Havbøye.py

--- Havbøye.py
+++ Havbøye.py
@@ -30,12 +30,12 @@ st.dataframe(df3)
 
 st.map(df3)
 
-st.header('Informasjon om bøye 4')
-st.dataframe(df4)
+#st.header('Informasjon om bøye 4')
+#st.dataframe(df4)
 
-st.map(df4)
+#st.map(df4)
 
-st.header('Informasjon om bøye 5')
-st.dataframe(df5)
+#st.header('Informasjon om bøye 5')
+#st.dataframe(df5)
 
-st.map(df5)
+#st.map(df5)
